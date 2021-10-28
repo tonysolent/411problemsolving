917e2fa7766390ddff690cd219e23e2cbdbfebf4
Tony Livins <5livit20@solent.ac.uk>
2021-10-28 10:34:22 +0100

    Created a basic input/output program.

---
+++ basics/Input/review.py
@@ -0,0 +1,14 @@
+# Display message to the standard output
+print("Hi..!! My name is Beep")
+print("I would like to ask some questions")
+print("May I ask some questions?")
+answer = input("Enter yes or no: ")
+if answer == "yes":
+    print("Hello")
+elif answer == "no":
+    Print("Hey")
+else:
+    print("Please enter yes or no.")
+print("What is your name human?")
+name = input()
+print(f"Hi {name} its nice to meet you")
