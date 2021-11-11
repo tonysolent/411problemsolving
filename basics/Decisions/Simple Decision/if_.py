ad9974ee545f526b1510ad68649bdb6600317a79
Tony Livins <5livit20@solent.ac.uk>
2021-11-11 10:30:52 +0000

    Added code to demonstrate the use of an if...else statement..

---
+++ basics/Decisions/Simple Decision/if_.py
@@ -0,0 +1,6 @@
+#Check if the user reads adventure books
+#Assume adventure is a variables
+adventure = input("What type of book is this?")
+if adventure == "adventure":
+    print("I like adventure books!")
+    print("Finished reading book")
---
+++ basics/Decisions/Simple Decision/if_else.py
@@ -0,0 +1,13 @@
+calculate = input("Please enter the activity to be performed?")
+print("")
+if calculate == "calculate":
+    print("")
+    print("Performing calculations...")
+    print("")
+    print("Finished reading book")
+    print("")
+else:
+    print("")
+    print("Performing activities...")
+    print("")
+    print("Activity completed!")
