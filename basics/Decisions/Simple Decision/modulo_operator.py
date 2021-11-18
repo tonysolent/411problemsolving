9b42f364f35bc84ee87ce0370dc01d61b60782e0
Tony Livins <5livit20@solent.ac.uk>
2021-11-18 09:59:05 +0000

    Added code to demonstrate the use of a modulus operator..

---
+++ basics/Decisions/Simple Decision/modulo_operator.py
@@ -0,0 +1,10 @@
+a=int(input("Please Enter a Number : "));
+if(a&1==1):  
+    print("This Number is Odd")    
+else:  
+    print("This Number is Even")
+a=int(input("Please Enter a Number : "));
+if(a%2==0):
+    print("This Number is Even")
+else:
+    print("This Number is Odd")
