57f4ffef11c5f816c6f6b0f99d22c5c7fcd2373b
Tony Livins <5livit20@solent.ac.uk>
2021-11-18 10:27:10 +0000

    Added code to demonstrate the use of comparison operators..

---
+++ basics/Decisions/Simple Decision/comparison_operators.py
@@ -0,0 +1,11 @@
+print("please enter the first number")
+first=float(input())
+print("please enter the second number")
+second=float(input())
+
+if first > second:
+    print("first is bigger than second")
+elif first < second:
+    print("second is bigger than first")
+else:
+    print("numbers are equal")
